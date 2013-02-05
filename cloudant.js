4b6c6c2900ade5c25618a7e17d8d3b283b6e88e0
Ronny Haase <ronhaase@gmail.com>
2013-02-05 06:31:41 +0100

    Initial commit

---
+++ cloudant.js
@@ -0,0 +1,54 @@
+function cloudant(username, password){
+	if ( !(this instanceof cloudant) ) return new cloudant(username. password);
+
+	this.allDbs = function() {
+		// https://_UN_:_PW_@_UN_.cloudant.com/_all_dbs
+	};
+}
+
+var Settings = {
+	limit
+	skip
+
+	key
+	startkey
+	endkey
+}
+
+cloudant.dB = function(dbname) {
+	if( !(this instanceof Db) ) return new Db(dbname);
+
+	function _exists() {
+		
+	}
+
+	// Creates the DB if it doesn't exist
+	this.create = function() {
+	}
+
+	// Deletes the DB
+	this.destroy = function() {
+	}
+
+	// Inserts / appends a new document to the DB
+	this.insert = function(jsondata) {
+	}
+
+	// Deletes a document of the DB
+	this.delete = function(_id, _rev) {
+	}
+
+	// Updates a document of the DB
+	this.update = function(_id, _rev, jsondata) {
+	}
+
+	this.query = function() {
+		// https://_UN_:_PW_@_UN_.cloudant.com/_DBNAME_/_all_docs
+	}
+	
+	// Queries a view of the DB
+	this.view = function(viewname) {
+		// https://_US_:_PW_@_UN_.cloudant.com/_DBNAME_/_design
+	};
+}
+
